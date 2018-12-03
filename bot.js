9e93a05e5653398c6be773ebd079a2cbd9632ecc
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-03 13:22:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,6 +36,22 @@ client.on('ready', () => {
   console.log('')
 });
 
+
+
+client.on('message', msg => {
+
+    if (msg.content == '*join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    });
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
